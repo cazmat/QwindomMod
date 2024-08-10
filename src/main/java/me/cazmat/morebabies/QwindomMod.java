d12f233036c6d0999cbf39fe59ce3d41bc9290c1
cazmat <93198094+cazmat@users.noreply.github.com>
2024-08-10 04:29:30 -0500

    Code restart

--- src/main/java/me/cazmat/morebabies/QwindomMod.java
+++ src/main/java/me/cazmat/morebabies/QwindomMod.java
@@ -1,7 +1,12 @@
 package me.cazmat.morebabies;
+
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
 @Mod(Constants.MOD_ID)
 public class QwindomMod {
     public QwindomMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
     }
 }
