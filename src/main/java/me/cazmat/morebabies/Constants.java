3f005ba4fc12c7ae024a682b552ea88f3263a7a0
cazmat <93198094+cazmat@users.noreply.github.com>
2024-08-10 04:23:47 -0500

    Move Mod ID to Constants class

---
+++ src/main/java/me/cazmat/morebabies/Constants.java
@@ -0,0 +1,5 @@
+package me.cazmat.morebabies;
+
+public class Constants {
+    public static final String MOD_ID = "morebabies";
+}
---
+++ src/main/java/me/cazmat/morebabies/QwindomMod.java
@@ -0,0 +1,7 @@
+package me.cazmat.morebabies;
+import net.minecraftforge.fml.common.Mod;
+@Mod(Constants.MOD_ID)
+public class QwindomMod {
+    public QwindomMod() {
+    }
+}
