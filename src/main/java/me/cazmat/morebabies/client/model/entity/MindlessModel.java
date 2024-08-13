d9191c7e0ee8058c2da56da448c24ee221f3513a
cazmat <93198094+cazmat@users.noreply.github.com>
2024-08-13 14:31:52 -0500

    Fix animation location

--- src/main/java/me/cazmat/morebabies/client/model/entity/MindlessModel.java
+++ src/main/java/me/cazmat/morebabies/client/model/entity/MindlessModel.java
@@ -8,7 +8,7 @@ import software.bernie.geckolib3.model.AnimatedGeoModel;
 public class MindlessModel extends AnimatedGeoModel<MindlessEntity> {
     private static final ResourceLocation modelResource = new ResourceLocation(Constants.MOD_ID, "geo/mindless.geo.json");
     private static final ResourceLocation textureResource = new ResourceLocation(Constants.MOD_ID, "textures/entity/mindless.png");
-    private static final ResourceLocation animationResource = new ResourceLocation(Constants.MOD_ID, "animations/entity/mindless.animation.json");
+    private static final ResourceLocation animationResource = new ResourceLocation(Constants.MOD_ID, "animations/mindless.animation.json");
     @Override
     public ResourceLocation getModelResource(MindlessEntity mindlessEntity) {
         return modelResource;
