c417aad39ae58bfa981b2ef54f6d2dcb15769457
cazmat <93198094+cazmat@users.noreply.github.com>
2024-08-16 03:11:57 -0500

    Config Switches to prevent Villager spawns

--- src/main/java/me/cazmat/morebabies/QwindomMod.java
+++ src/main/java/me/cazmat/morebabies/QwindomMod.java
@@ -4,12 +4,19 @@ import me.cazmat.morebabies.config.ConfigManager;
 import me.cazmat.morebabies.proxy.ClientProxy;
 import me.cazmat.morebabies.proxy.CommonProxy;
 import me.cazmat.morebabies.registry.EntityRegistry;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
 
@@ -22,13 +29,40 @@ public class QwindomMod {
         ConfigManager.loadConfig(ConfigManager.SPEC, FMLPaths.CONFIGDIR.get().resolve(Constants.MOD_ID + "-common.toml"));
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        modEventBus.addListener(this::setup);
         modEventBus.addListener(this::setupClient);
 
         EntityRegistry.DEF_REG.register(modEventBus);
 
         PROXY.init();
     }
+    private void setup(FMLLoadCompleteEvent event) {
+        MinecraftForge.EVENT_BUS.addListener(this::checkEntitySpawn);
+        MinecraftForge.EVENT_BUS.addListener(this::checkSpecialEntitySpawn);
+    }
     private void setupClient(FMLClientSetupEvent event) {
         PROXY.clientInit();
+    }
+    private void checkEntitySpawn(LivingSpawnEvent.CheckSpawn event) {
+        if(event.getEntity() instanceof Villager) {
+            if(!ConfigManager.allowVillagerSpawns()) {
+                event.getEntity().remove(Entity.RemovalReason.DISCARDED);
+                event.setResult(Event.Result.DENY);
+            }
+        }
+    }
+    private void checkSpecialEntitySpawn(LivingSpawnEvent.SpecialSpawn event) {
+        if(event.getEntity() instanceof Villager) {
+            if(!ConfigManager.allowVillagerSpawns()) {
+                if((event.getSpawnReason() == MobSpawnType.SPAWN_EGG) && (ConfigManager.allowVillagerEggSpawn())) {
+                    return;
+                }
+                if((event.getSpawnReason() == MobSpawnType.COMMAND) && (ConfigManager.allowVillagerCommandSpawn())) {
+                    return;
+                }
+                event.getEntity().remove(Entity.RemovalReason.DISCARDED);
+                event.setCanceled(true);
+            }
+        }
     }
 }
--- src/main/java/me/cazmat/morebabies/config/ConfigManager.java
+++ src/main/java/me/cazmat/morebabies/config/ConfigManager.java
@@ -14,9 +14,15 @@ import java.nio.file.Path;
 public class ConfigManager {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
+    private static final ForgeConfigSpec.BooleanValue VILLAGER_SPAWNS_ENABLED;
+    private static final ForgeConfigSpec.BooleanValue VILLAGER_SPAWNS_COMMAND;
+    private static final ForgeConfigSpec.BooleanValue VILLAGER_SPAWNS_EGG;
 
     static {
-        BUILDER.push(Constants.MOD_ID);
+        BUILDER.push("Mob Spawns");
+        VILLAGER_SPAWNS_ENABLED = BUILDER.comment("Do we allow villager spawns on the server?").define("villager_spawns_enabled", false);
+        VILLAGER_SPAWNS_COMMAND = BUILDER.comment("Do we allow commands to spawn villagers? (Overrides villager spawn setting)").define("villager_spawns_command", true);
+        VILLAGER_SPAWNS_EGG = BUILDER.comment("Do we allow spawn eggs to spawn villagers? (Overrides villager spawn setting)").define("villager_spawns_egg", true);
         BUILDER.pop();
         SPEC = BUILDER.build();
     }
@@ -24,5 +30,14 @@ public class ConfigManager {
         CommentedFileConfig configData = CommentedFileConfig.builder(path).sync().autosave().writingMode(WritingMode.REPLACE).build();
         configData.load();
         spec.setConfig(configData);
+    }
+    public static boolean allowVillagerSpawns() {
+        return VILLAGER_SPAWNS_ENABLED.get();
+    }
+    public static boolean allowVillagerCommandSpawn() {
+        return VILLAGER_SPAWNS_COMMAND.get();
+    }
+    public static boolean allowVillagerEggSpawn() {
+        return VILLAGER_SPAWNS_EGG.get();
     }
 }
