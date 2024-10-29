4ce9dca7473d3478e8fd0e4745f352665c9a1fe8
cazmat <93198094+cazmat@users.noreply.github.com>
2024-10-28 20:12:06 -0500

    Calcite Wall

--- src/main/java/me/cazmat/qwindom/registry/BlockRegistry.java
+++ src/main/java/me/cazmat/qwindom/registry/BlockRegistry.java
@@ -5,6 +5,9 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.WallBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,6 +17,9 @@ import java.util.function.Supplier;
 
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Qwindom.MOD_ID);
+
+    public static final RegistryObject<Block> CALCITE_WALL = registerBlock("calcite_wall",
+            () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.CALCITE)), TabsRegistry.WALLS_TAB);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/me/cazmat/qwindom/registry/TabsRegistry.java
+++ src/main/java/me/cazmat/qwindom/registry/TabsRegistry.java
@@ -1,8 +1,16 @@
 package me.cazmat.qwindom.registry;
 
 import me.cazmat.qwindom.Qwindom;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Qwindom.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TabsRegistry {
+    public static final CreativeModeTab WALLS_TAB = new CreativeModeTab("ctvcore_walls") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(BlockRegistry.CALCITE_WALL.get());
+        }
+    };
 }
