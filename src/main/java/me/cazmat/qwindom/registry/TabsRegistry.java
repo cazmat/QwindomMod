0f3b3b0f90f9a85a7c10dd48f580b3d341f1f2f3
cazmat <93198094+cazmat@users.noreply.github.com>
2024-10-28 20:01:58 -0500

    Create TabsRegistry

---
+++ src/main/java/me/cazmat/qwindom/registry/TabsRegistry.java
@@ -0,0 +1,8 @@
+package me.cazmat.qwindom.registry;
+
+import me.cazmat.qwindom.Qwindom;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Qwindom.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class TabsRegistry {
+}
